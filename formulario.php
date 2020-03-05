ea03bf03371e1778c5bcc54602b6118b85e67b2d
305 <305@academicos.cesde.edu.co>
2020-03-04 21:01:50 -0500

    V0.2

---
+++ formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Bootstrap Form with jQuery Validations</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
+
+</head>
+
+<body>
+https://programacion.net/articulo/disenando_un_formulario_de_registro_en_bootstrap_con_validacion_de_campos_jquery_1535
+</body>
+</html>
